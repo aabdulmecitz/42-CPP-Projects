6872e8f97cea449c9a79c43eaaba54e1b7ce5e97
Ahmet abdülmecit Özkaya <aozkaya@k1m43s05.42istanbul.com.tr>
2025-06-01 15:13:28 +0300

    this

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -12,3 +12,56 @@ void Account::displayAccountsInfos( void )
 }
 
 
+int Account::getNbAccounts( void )
+{
+    return _nbAccounts;
+}
+int Account::getTotalAmount( void )
+{
+    return _totalAmount;
+}
+int Account::getNbDeposits( void )
+{
+    return _totalNbDeposits;
+}
+int Account::getNbWithdrawals( void )
+{
+    return _totalNbWithdrawals;
+}
+
+
+Account::Account( int initial_deposit )
+{
+    
+}
+
+Account::~Account( void )
+{
+
+}
+
+void Account::displayAccountsInfos( void )
+{
+
+}
+
+
+void Account::makeDeposit(int deposit)
+{
+
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int		Account::checkAmount( void ) const
+{
+
+}
+
+void	Account::displayStatus( void ) const
+{
+
+}
