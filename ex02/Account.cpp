616a46c91c06f14b5a659e92c03aea481e6841a0
Ahmet abdülmecit Özkaya <aozkaya@k1m43s05.42istanbul.com.tr>
2025-06-01 18:33:05 +0300

    hsdfgsd

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,16 +1,14 @@
 #include "Account.hpp"
 
-int Account::_nbAccounts = 0;
-int Account::_totalAmount = 0;
-int Account::_totalNbDeposits = 0;
-int Account::_totalNbWithdrawals = 0;
-
+// int Account::_nbAccounts = 0;
+// int Account::_totalAmount = 0;
+// int Account::_totalNbDeposits = 0;
+// int Account::_totalNbWithdrawals = 0;
 
 void Account::displayAccountsInfos( void )
 {
 
 }
-
 
 int Account::getNbAccounts( void )
 {
@@ -32,23 +30,27 @@ int Account::getNbWithdrawals( void )
 
 Account::Account( int initial_deposit )
 {
-    
+    _nbAccounts++;
+    _amount = initial_deposit;
+    _nbDeposits = 0;
+    _nbWithdrawals = 0;
+    _totalAmount += initial_deposit;
 }
 
 Account::~Account( void )
 {
-
+    _nbAccounts--;
+    _totalAmount -= _amount;
+    _totalNbDeposits -= _nbDeposits;
+    _totalNbWithdrawals -= _nbWithdrawals;
 }
-
-void Account::displayAccountsInfos( void )
-{
-
-}
-
 
 void Account::makeDeposit(int deposit)
 {
-
+    _totalNbDeposits++;
+    _amount += deposit;
+    _nbDeposits++;
+    _totalAmount += deposit;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
@@ -58,7 +60,7 @@ bool Account::makeWithdrawal(int withdrawal)
 
 int		Account::checkAmount( void ) const
 {
-
+    return _amount;
 }
 
 void	Account::displayStatus( void ) const
