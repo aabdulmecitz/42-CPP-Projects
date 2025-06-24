70b008fc6ecfa3fcaa35eaa7ca5a6f40530fa931
Ahmet abdülmecit Özkaya <aozkaya@k1m43s01.42istanbul.com.tr>
2025-06-24 21:04:59 +0300

    qwerty

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,28 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+    
+private:
+    int _rawBits;
+    static const int _fractionalBits = 8;
+public:
+    Fixed(void);
+    ~Fixed(void);
+    Fixed(const Fixed& other);
+    Fixed& operator=(const Fixed& other);
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+
+    Fixed(int const value);
+    Fixed(float const value);
+    float toFloat(void) const;
+    int toInt(void) const;
+
+    Fixed& operator<<(const Fixed& other) const;
+};
+
+#endif
