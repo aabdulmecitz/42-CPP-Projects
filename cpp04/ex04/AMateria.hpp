ee59bd544e858266ac8384e353938c99f236b138
aabdulmecitz <ahmetabdulmecid@gmail.com>
2025-07-21 15:48:52 +0300

    this

---
+++ cpp04/ex04/AMateria.hpp
@@ -0,0 +1,17 @@
+#ifndef AMETERIA_HPP
+# define AMETERIA_HPP
+#include <iostream>
+#include "ICharacter.hpp"
+
+class AMateria
+{
+    protected:
+
+    public:
+        AMateria(std::string const & type);
+        std::string const & getType() const; //Returns the materia type
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+};
+
+#endif
