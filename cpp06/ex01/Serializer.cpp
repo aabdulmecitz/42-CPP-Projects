5d426dfa95e40671ed9f7d590e0b1e39e6b0fb67
aabdulmecitz <ahmetabdulmecid@gmail.com>
2025-11-03 15:03:58 +0300

    thiss

--- cpp06/ex01/Serializer.cpp
+++ cpp06/ex01/Serializer.cpp
@@ -20,12 +20,12 @@ Serializer::~Serializer() {}
 
 uintptr_t Serializer::serialize(Data *ptr)
 {
-    return (uintptr_t)ptr;
+    return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data *Serializer::deserialize(uintptr_t raw)
 {
-    return (Data *)raw;
+    return reinterpret_cast<Data *>(raw);
 }
 
 std::ostream& operator<<(std::ostream& os, const Serializer& sc)
