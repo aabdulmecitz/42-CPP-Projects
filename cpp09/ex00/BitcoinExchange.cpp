5b9a5a79f1ec30669d1b9fbdddcf5621e28af43a
aabdulmecitz <ahmetabdulmecid@gmail.com>
2025-11-16 01:55:58 +0300

    ttt

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -31,12 +31,22 @@ bool BitcoinExchange::isValidDate(const std::string& date)
             return false;
     }
     
+    int year = std::atoi(date.substr(0, 4).c_str());
     int month = std::atoi(date.substr(5, 2).c_str());
     int day = std::atoi(date.substr(8, 2).c_str());
     
     if (month < 1 || month > 12)
         return false;
-    if (day < 1 || day > 31)
+    
+    // Days in each month
+    int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    // Check for leap year
+    bool isLeapYear = (year % 4 == 0);
+    if (isLeapYear)
+        daysInMonth[2] = 29;
+    
+    if (day < 1 || day > daysInMonth[month])
         return false;
     
     return true;
