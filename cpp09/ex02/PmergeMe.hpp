ef5ea8d7bfc78aee70c5212d32e31460384b309d
aabdulmecitz <ahmetabdulmecid@gmail.com>
2025-11-16 04:18:42 +0300

    thiss

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,13 +6,17 @@
 # include <stack>
 # include <sstream>
 # include <vector>
+# include <deque>
 # include <cctype>
 # include <cerrno>
 # include <climits>
 # include <cstdlib>
+# include <algorithm>
+# include <ctime>
 
 std::vector<int>    init_vector(int argc, char *argv[]);
 void                print_vector(const std::vector<int>& vec);
+void                print_datas(std::vector<int>& numbers);
 
 
 #endif
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,4 +1,78 @@
 #include "./PmergeMe.hpp"
+#include <ctime>
+#include <iomanip>
+
+// Color codes for terminal output
+#define COLOR_RESET     "\033[0m"
+#define COLOR_BOLD      "\033[1m"
+#define COLOR_CYAN      "\033[36m"
+#define COLOR_GREEN     "\033[32m"
+#define COLOR_YELLOW    "\033[33m"
+#define COLOR_MAGENTA   "\033[35m"
+#define COLOR_BLUE      "\033[34m"
+
+void print_datas(std::vector<int>& numbers)
+{
+    const size_t MAX_DISPLAY = 10;
+    
+    // Display "Before" line
+    std::cout << COLOR_CYAN << COLOR_BOLD << "Before: " << COLOR_RESET;
+    for (size_t i = 0; i < numbers.size(); i++)
+    {
+        if (i > 0)
+            std::cout << " ";
+        if (i < MAX_DISPLAY)
+            std::cout << COLOR_YELLOW << numbers[i] << COLOR_RESET;
+        else if (i == MAX_DISPLAY)
+        {
+            std::cout << COLOR_MAGENTA << "[...]" << COLOR_RESET;
+            break;
+        }
+    }
+    std::cout << std::endl;
+    
+    // Vector: measure total time including data management and sorting
+    clock_t start_vector = clock();
+    std::vector<int> sorted_vector = numbers;
+    std::sort(sorted_vector.begin(), sorted_vector.end());
+    clock_t end_vector = clock();
+    
+    // Display "After" line
+    std::cout << COLOR_GREEN << COLOR_BOLD << "After: " << COLOR_RESET;
+    for (size_t i = 0; i < sorted_vector.size(); i++)
+    {
+        if (i > 0)
+            std::cout << " ";
+        if (i < MAX_DISPLAY)
+            std::cout << COLOR_GREEN << sorted_vector[i] << COLOR_RESET;
+        else if (i == MAX_DISPLAY)
+        {
+            std::cout << COLOR_MAGENTA << "[...]" << COLOR_RESET;
+            break;
+        }
+    }
+    std::cout << std::endl;
+    
+    // Calculate total time for vector (data management + sorting) in microseconds
+    double time_vector = static_cast<double>(end_vector - start_vector) / CLOCKS_PER_SEC * 1000000;
+    
+    std::cout << std::fixed << std::setprecision(5);
+    std::cout << COLOR_BLUE << "Time to process a range of " << sorted_vector.size() 
+              << " elements with " << COLOR_BOLD << "std::vector" << COLOR_RESET << COLOR_BLUE 
+              << " : " << COLOR_YELLOW << time_vector << COLOR_BLUE << " us" << COLOR_RESET << std::endl;
+    
+    // Deque: measure total time including data management and sorting
+    clock_t start_deque = clock();
+    std::deque<int> sorted_deque(numbers.begin(), numbers.end());
+    std::sort(sorted_deque.begin(), sorted_deque.end());
+    clock_t end_deque = clock();
+    
+    // Calculate total time for deque (data management + sorting) in microseconds
+    double time_deque = static_cast<double>(end_deque - start_deque) / CLOCKS_PER_SEC * 1000000;
+    std::cout << COLOR_BLUE << "Time to process a range of " << sorted_deque.size() 
+              << " elements with " << COLOR_BOLD << "std::deque" << COLOR_RESET << COLOR_BLUE 
+              << " : " << COLOR_YELLOW << time_deque << COLOR_BLUE << " us" << COLOR_RESET << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
@@ -6,7 +80,7 @@ int main(int argc, char *argv[])
     if (numbers.empty())
         return 1;
     
-    print_vector(numbers);
+    print_datas(numbers);
     
     return 0;
 }
